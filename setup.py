4d3afa1b4087b2f6b3e1cf5dbb2e9f87e6c38cba
Kiminori Kitahata <kitahata@rikinet.co.jp>
2017-09-11 16:35:04 +0900

    インストールできることを確認した

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name='telnet_driver',
       author='Riki Network Systems, Inc.',
       author_email='kitahata@rikinet.co.jp',
       license='Apache License Version 2.0',
-      packages=find_packages(exclude='test_*'),
+      packages=find_packages(),
       zip_safe=False,)
